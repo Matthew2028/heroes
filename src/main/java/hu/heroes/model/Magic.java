4057ff5f83cd887e0c05fa2bed0f1dc7d89750bb
Máté Varga <varmat28@gmail.com>
2022-04-08 21:56:02 +0200

    Refactor Hero

---
+++ src/main/java/hu/heroes/model/Magic.java
@@ -0,0 +1,13 @@
+package hu.heroes.model;
+
+public class Magic {
+    private String name;
+    private int price;
+    private int manaCost;
+
+    public Magic(String name, int price, int manaCost) {
+        this.name = name;
+        this.price = price;
+        this.manaCost = manaCost;
+    }
+}
